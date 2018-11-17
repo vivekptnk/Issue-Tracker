b1313d887f6963dac2b523245c7b67313913f5db
Vivek Pattanaik <vivekpatanaik100@gmail.com>
2018-11-17 23:11:18 +0530

    main.js update

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ function saveIssue(e){
     var issueDec = document.getElementById('issueDescInput').value;
     var issueSeverity = document.getElementById('issueSeverityInput').value;
     var issueAssignedTo = document.getElementById('issueAssignedToInput').value;
+    var issueId = chance.guid();
     
 
 }
